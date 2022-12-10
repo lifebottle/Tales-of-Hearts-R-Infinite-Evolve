2286e0204d7eb59d76ea8a6ec2262e9116ed37d7
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2022-12-10 02:40:12 -0500

    commit

---
+++ _recompile.bat
@@ -0,0 +1,7 @@
+python recompile.py 0_tohdata_dir 2_translated 3_patched
+
+pushd "tools\Asm"
+armips.exe HeartsIE.asm
+popd
+
+pause
--- recompile.py
+++ recompile.py
@@ -1,4 +1,4 @@
-import toir
+import tohie
 import click
 from pathlib import Path
 
@@ -23,12 +23,12 @@ def extract(source, newtext, destination):
     destl7cdir = destination / 'tohdata_release'
     destl7cdir.mkdir(parents=True, exist_ok=True)
 
-    #toir.recompile_eboot(ebootpath, newtext, destination)
-    #toir.recompile_dat(l7cdir, newtext, destl7cdir)
-    toir.recompile_skits(l7cdir, newtext, destl7cdir)
-    #toir.recompile_story(l7cdir, newtext, destl7cdir)
-    #toir.recompile_map_data(l7cdir, newtext, destl7cdir)
-    #toir.recompile_textures(l7cdir, newtext, destl7cdir)
+    #tohie.recompile_eboot(ebootpath, newtext, destination)
+    #tohie.recompile_dat(l7cdir, newtext, destl7cdir)
+    tohie.recompile_skits(l7cdir, newtext, destl7cdir)
+    #tohie.recompile_story(l7cdir, newtext, destl7cdir)
+    #tohie.recompile_map_data(l7cdir, newtext, destl7cdir)
+    tohie.recompile_textures(l7cdir, newtext, destl7cdir)
 
 if __name__ == '__main__':
     extract()
