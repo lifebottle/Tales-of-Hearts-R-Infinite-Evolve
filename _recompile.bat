b646bf8e6034224d3e766e992e5765fa0fcc5aee
FistingUranus <77898683+Fis-Ura@users.noreply.github.com>
2022-12-10 22:49:26 -0500

    Added a google sheet fetcher to the Recompile

--- _recompile.bat
+++ _recompile.bat
@@ -1,3 +1,6 @@
+curl -L "https://docs.google.com/spreadsheets/d/1PFX-TU-6UQuQHEk8Hv91UqJG-OWEo1T6mVx-8tZ4O4Q/gviz/tq?tqx=out:csv&sheet=Skit&range=A:H" > "2_translated/Skit.csv"
+
+
 python recompile.py 0_tohdata_dir 2_translated 3_patched
 
 pushd "tools\Asm"
